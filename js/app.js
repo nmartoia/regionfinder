d5a334f9ea739d20550a29ad230868d01980d7e2
nmartoia <nmartoia@edenschool.fr>
2022-06-03 15:19:14 +0200

    finish project

--- js/app.js
+++ js/app.js
@@ -12,21 +12,31 @@ selectR.addEventListener('change',()=>{
     }
     dd.textContent='selectionnez un departement'
     dd.style.display='block'
-    selectd.options=dd
+    selectd.selectedIndex=0
     if(selectR.options[0].selected===true){
         selectd.options[1].textContent=nomd[0];
+        selectd.options[1].value=nomd[0];
         selectd.options[2].textContent=nomd[1];
+        selectd.options[2].value=nomd[1];
         selectd.options[3].textContent=nomd[2];
+        selectd.options[3].value=nomd[2];
         selectd.options[3].style.display='block';
     }else if(selectR.options[1].selected===true){
         selectd.options[1].textContent=nomd[3];
+        selectd.options[1].value=nomd[3];
         selectd.options[2].textContent=nomd[4];
+        selectd.options[2].value=nomd[4];
         selectd.options[3].style.display='none';
+        selectd.options[3].value='';
+        selectd.options[3].textContent='';
     }
     else{
         selectd.options[1].textContent=nomd[5];
+        selectd.options[1].value=nomd[5];
         selectd.options[2].textContent=nomd[6];
+        selectd.options[2].value=nomd[6];
         selectd.options[3].textContent=nomd[7];
+        selectd.options[3].value=nomd[7];
         selectd.options[3].style.display='block';
     }
 })
